24f7bc21738c544254034318aabb145fda7981f5
Yeliena <elena.titova@metricinsights.com>
2024-06-23 13:20:25 +0200

    first part of homework

---
+++ home6.js
@@ -0,0 +1,11 @@
+function myBlend(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+myBlend(arr);
+console.log(arr);
